9c669554f25f4d111fa85d064ed83beaef5ee633
Chris Kavanagh <ckava3@gmail.com>
2019-08-25 04:03:23 -0400

    jsonwebtoken

--- controllers/user.js
+++ controllers/user.js
@@ -5,6 +5,8 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const { User, validate } = require("../model/user");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -38,4 +40,35 @@ exports.delUserController = async (req, res, next) => {
   const user = await User.findByIdAndRemove(req.params.userId);
   if (!user) return res.status(404).send("The user was not found.");
   res.status(200).json({ message: "User Deleted", user: user });
+};
+
+exports.loginController = async (req, res, next) => {
+  const user = await User.find({ email: req.body.email });
+  console.log(user);
+  if (user < 1) {
+    return res.status(401).json({ message: "Authentication Failed" });
+  }
+
+  bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+    if (err) {
+      res.status(401).json({ message: "Auth Failed" });
+    }
+    if (result) {
+      const token = jwt.sign(
+        {
+          email: user[0].email,
+          userId: user[0]._id
+        },
+        config.get("JWT_KEY"),
+        {
+          expiresIn: "1h"
+        }
+      );
+      return res.status(200).json({
+        message: "Auth Successful",
+        token: token
+      });
+    }
+    res.status(401).json({ message: "Auth Failed" });
+  });
 };
--- routes/user.js
+++ routes/user.js
@@ -3,10 +3,13 @@ require("express-async-errors");
 const router = express.Router();
 const {
   userCreateController,
-  delUserController
+  delUserController,
+  loginController
 } = require("../controllers/user");
 
 router.post("/signup", userCreateController);
+
+router.post("/login", loginController);
 
 router.delete("/:userId", delUserController);
 
